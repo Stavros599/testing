9f4d8a211a2505b8229e28800491d2882739fa97
Stavros Nicolaou <up1069660@upatras.gr>
2023-05-25 14:39:23 +0300

    proto

---
+++ studylog/RegistrationForm.java
@@ -0,0 +1,11 @@
+package sutdylog;
+public class RegistrationForm {
+    String onoma;
+    String epitheto;
+    String passward;
+    int age;
+    int ID;
+    public RegistrationForm(){
+        ID=0;
+    }
+}
---
+++ studylog/User.java
@@ -0,0 +1,27 @@
+package sutdylog;
+public class User {
+    String onoma;
+    String epitheto;
+    String passward;
+    int age;
+    int ID;
+    int kathgoria;
+    int arithmosMathimaton;
+    int arithmosProtereotitas;
+    int aritmosQuiz;
+    public User(RegistrationForm dilosoulaTreloula){
+        this.onoma=dilosoulaTreloula.onoma;
+        this.epitheto=dilosoulaTreloula.epitheto;
+        this.passward=dilosoulaTreloula.passward;
+        this.ID=dilosoulaTreloula.ID;
+        kathgoria=0;
+        arithmosMathimaton=1;
+        arithmosProtereotitas=0;
+        aritmosQuiz=1;
+    }
+    public void setkathgoria(int paketo){
+        if(paketo==0){}
+        else if(paketo==1){}
+        else if(paketo==2){}       // edo prosarmozoume ta paketa
+    }
+}
